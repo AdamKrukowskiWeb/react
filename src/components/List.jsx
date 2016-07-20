58493f9e6b87e2f8b8e643baf0bb6a2b55c5e689
Adam Krukowski <adam.krukowski.adam@gmail.com>
2016-07-20 18:39:42 +0200

    src

---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ListItems = reuqire('./ListItems.jsx');
+
+var ingredients = [{"id":1, "text":"ham"}, {"id":2, "text":"ser"}, {"id":3,}, "text":"ziemniaki"];
+
+var List = React.createClass({
+  render: function () {
+    var ListItems = ingredients.map(function (item) {
+
+    })
+  }
+});
